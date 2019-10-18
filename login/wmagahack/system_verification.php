34c775628298ff5f57f398355b715bfb9bc7eb37
ReeceHaigh <55700746+ReeceHaigh@users.noreply.github.com>
2019-10-18 12:18:19 +0100

    Add files via upload

---
+++ login/wmagahack/system_verification.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Gotham City Digital - Sharing Gotham City With The World</title>
+<link href="css/master.css" rel="stylesheet" />
+</head>
+
+<body>
+
+<div id="container">
+	<div id="header">
+		<div id="logout">
+					</div>
+	</div>
+		<div id="content">		<script type="text/javascript" src="js/swfobject.js"></script>
+		<script type="text/javascript">
+			swfobject.registerObject("video", "9.0.0");
+		</script>
+		<div style="text-align:center;">
+			<br>
+			<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="320" height="240" id="sys">
+				<param name="movie" value="sys.swf" />
+				<!--[if !IE]>-->
+				<object type="application/x-shockwave-flash" data="sys.swf" width="320" height="240">
+				<!--<![endif]-->
+					<a href="http://www.adobe.com/go/getflashplayer">
+						<img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" />
+
+					</a>
+				<!--[if !IE]>-->
+				</object>
+				<!--<![endif]-->
+			</object>
+		</div>
+	</div>
+	<br>
+		<br>
+</div>
+
+<table width="500" id="footerlinks" align="center" cellpadding="5">
+	<tr>
+		<td>Contact Us</td>
+		<td>About Us</td>
+		<td>Privacy</td>
+		<td>Terms of Service</td>
+		<td>Careers</td>
+	</tr>
+</table>
+<br />
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-9898474-3");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</body>
+</html>
